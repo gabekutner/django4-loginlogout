6e33ddadd1e601406112ef7ea10c6be36c988dc6
gabekutner <108239517+gabekutner@users.noreply.github.com>
2023-04-02 17:23:31 -0700

    update 4/2
    password visibility eye

--- logproject/logproject/site_static/site/js/login.js
+++
@@ -1,8 +0,0 @@
-function toggleVisibility() {
-    var getPassword = document.getElementById('pswrd');
-    if(getPassword.type === 'password') {
-        getPassword.type = 'text';
-    } else {
-        getPassword.type = 'password';
-    }
-}
